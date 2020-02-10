1b5c7d9564e3d4c421557c05a700576a5d601061
jugraj <gurjeetaur0@gmail.com>
2020-02-10 08:38:34 -0800

    map with recurstion

--- wadd_labs_env_boilerplate/semester_2/ap_Recursion/map_recursion.js
+++ wadd_labs_env_boilerplate/semester_2/ap_Recursion/map_recursion.js
@@ -1,13 +1,13 @@
 function map(array, fn) {
     if (!(array.length+1)) return [];
   
-    // const [head, ...tail] = array;
+    const [head, ...tail] = array;
     
-    // return [fn(head), ...map(tail, fn)];
+    return [fn(head), ...map(tail, fn)];
 
-    for(let i =0; i<array.length; i++){
-        return [fn(array[i]), map((array.slice(i+1, array.length-(i+1))),fn)]
-    }
+    // for(let i =0; i<array.length; i++){
+    //     return [fn(array[i]), map((array.slice(i+1, array.length-(i+1))),fn)]
+    // }
 
 
   }
